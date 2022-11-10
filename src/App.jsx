82e848ba518f91067e12f01bbab58d8d64ac4ba9
Abhii30 <akumar300599@gmail.com>
2022-11-10 21:48:55 +0530

    add autocomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {
   useJsApiLoader,
   Marker,
   GoogleMap,
   Autocomplete,
+  DirectionsRenderer,
 } from "@react-google-maps/api";
 import styles from "./App.module.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const center = { lat: 24.7914, lng: 85.0002 };
+const libraries = ["places"];
 
 function App() {
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: "AIzaSyANDj4mJ2i_d0zRiNozVqbUGMgZTnDOeYI",
-    libraries: ["places"],
+    libraries,
   });
 
   const [map, setMap] = useState(/** @type google.map.Map */ (null));
+  const [directionResponse, setDirectionResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+
+  const originRef = useRef();
+  const destinationRef = useRef();
 
   if (!isLoaded) {
     return <h1>Loading...</h1>;
   }
+
+  async function calculateRoute() {
+    if (originRef.current.value === "" || destinationRef.current.value === "") {
+      return;
+    }
+    const directionService = new window.google.maps.DirectionsService();
+    const result = await directionService.route({
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      travelMode: window.google.maps.TravelMode.DRIVING,
+    });
+    setDirectionResponse(result);
+    setDistance(result.routes[0].legs[0].distance.text);
+    setDuration(result.routes[0].legs[0].duration.text);
+  }
+
+  function clearRoute() {
+    setDistance("");
+    setDuration("");
+    setDirectionResponse(null);
+    originRef.current.value = "";
+    destinationRef.current.value = "";
+  }
+
   return (
     <div className={styles.App}>
       <header>Hello</header>
@@ -40,6 +72,9 @@ function App() {
             onLoad={(map) => setMap(map)}
           >
             <Marker position={center} />
+            {directionResponse && (
+              <DirectionsRenderer directions={directionResponse} />
+            )}
           </GoogleMap>
         </div>
         <div className="col-6">
@@ -47,16 +82,28 @@ function App() {
             <div className="form-group col-md-6">
               <label for="">Origin</label>
               <Autocomplete>
-                <input type="text" className="form-control form-control-lg" />
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  ref={originRef}
+                />
               </Autocomplete>
             </div>
-            <button className="btn btn-primary mb-2 " id="calc">
+            <button
+              className="btn btn-primary mb-2 "
+              id="calc"
+              onClick={calculateRoute}
+            >
               Calculate
             </button>
             <div className="form-group mt-5 col-md-6">
               <label for="">Destination</label>
               <Autocomplete>
-                <input type="text" className="form-control form-control-lg" />
+                <input
+                  type="text"
+                  className="form-control form-control-lg"
+                  ref={destinationRef}
+                />
               </Autocomplete>
             </div>
             <button
@@ -65,9 +112,19 @@ function App() {
             >
               Recenter
             </button>
+            <button
+              className="btn btn-danger mt-4"
+              id="calc"
+              onClick={clearRoute}
+            >
+              Clear
+            </button>
           </form>
           <div className={styles.container}>
-            <h1>Distance</h1>
+            <h1>Distance : {distance}</h1>
+          </div>
+          <div className={styles.container}>
+            <h1>Duration : {duration}</h1>
           </div>
         </div>
       </div>
